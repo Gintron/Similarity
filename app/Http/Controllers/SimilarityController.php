610501ca39569dc4a8434f90e5c46408fc302f93
gintron <marijan10vk@gmail.com>
2021-09-06 19:32:08 +0200

    gettin data from bacon API

--- app/Http/Controllers/SimilarityController.php
+++ app/Http/Controllers/SimilarityController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Http;
 class SimilarityController extends Controller
 {
     public function index(){
-       
+       dd($this->getDataFromBaconApi());
         return view('similarity.index');
+    }
+
+    public function getDataFromBaconApi(){
+        return Http::get('https://baconipsum.com/api/?type=all-meat&sentences=2&start-with-lorem=0');
     }
 }
